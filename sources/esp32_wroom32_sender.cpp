98ca4c59a7819a4b471ec349844e265efb785cfb
Szabó Gergő <lest12110@gmail.com>
2025-04-01 22:08:18 +0200

    esp32wroom32

---
+++ sources/esp32_wroom32_sender.cpp
@@ -0,0 +1,86 @@
+/*
+ * Developed by CyberHorizon
+ * GitHub: https://github.com/cyberhorizonltd
+ * Website: https://cyberhorizon.hu
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from its use.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it freely,
+ * subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not claim
+ *    that you wrote the original software. If you use this software in a product,
+ *    an acknowledgment in the product documentation would be appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such and must not be misrepresented
+ *    as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include <string>
+#include <Wire.h>
+
+#define VIRTUAL_DATA_LINE 5
+#define VIRTUAL_CLOCK_LINE 18
+#define VIRTUAL_ENABLE_LINE 19
+#define VIRTUAL_NOTIFY_LINE 21
+ 
+ void notif() {
+     digitalWrite(VIRTUAL_NOTIFY_LINE, 1);
+ }
+ 
+ void stopnotif() {
+     digitalWrite(VIRTUAL_NOTIFY_LINE, 0);
+ }
+ 
+ void write(bool bit)
+ {
+     digitalWrite(VIRTUAL_DATA_LINE, bit);
+ }
+ 
+ void clock(bool state)
+ {
+     digitalWrite(VIRTUAL_CLOCK_LINE, state);
+ }
+ 
+ void waitForEnab() {
+     while (digitalRead(VIRTUAL_ENABLE_LINE) == LOW) ;
+ }
+ void waitForDisab() {
+     while (digitalRead(VIRTUAL_ENABLE_LINE) == HIGH) ;
+ }
+ 
+ void char_to_bool_array(char c, bool *bits)
+ {
+     for (int i = 0; i < 8; i++)
+     {
+         bits[i] = (c >> i) & 1;
+     }
+ }
+ 
+ void sendChar(char c)
+ {
+     bool bits[8];
+     char_to_bool_array(c, bits);
+     for (int j = 0; j < 8; j++)
+     {
+         write(false);
+         clock(false);
+         waitForEnab();
+         write(bits[j]);
+         clock(true);
+         waitForDisab();
+     }
+     clock(false);
+ }
+ 
+ void sendString(std::string data)
+ {
+     data += '\n';
+     notif();
+     for (int i = 0; i < data.length(); i++)
+     {
+         sendChar(data[i]);
+     }
+     stopnotif();
+ }
